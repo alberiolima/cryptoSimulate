e20ca0fd0b940f9c77dbc4bff06cb8224c61f867
Alberio Lima <alberio@dadox.com.br>
2022-05-02 09:07:09 -0300

    Add thread and config file

--- SiriCoinSerialMinerIOT/SiriCoinSerialMinerIOT.py
+++ SiriCoinSerialMinerIOT/SiriCoinSerialMinerIOT.py
@@ -2,22 +2,54 @@ import time, serial
 import socket
 import sys
 from colorama import Fore
+import os.path 
+from threading import Thread
+import configparser
+
+configMinerName = 'config.ini'
+configMiner = configparser.ConfigParser()
 
 #change usb-serial port (baud=115200)
-serialPort = "/dev/ttyACM0"
+serialPorts = ""
 
 #miner id
-minerAddr = "0x0E9b419F7Cd861bf86230b124229F9a1b6FF9674"
+minerAddr = ""
 minerDiff = 1000
 
 #client TCP
-HOST, PORT = "192.168.1.36", 9999
+HOST, PORT = "192.168.1.33", 9999
 
 results_good = 0
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while True:
-	#Conecta com servidor TCP
+def readConfigMiner():
+    configMiner['DEFAULT'] = {'miner_addr': '', 'serial_ports': '','time_work': 20 }
+    if (os.path.exists(configMinerName) is False):
+        writeConfigMiner()
+    global minerAddr,serialPorts,tempoTrabalhar
+    try:        
+        configMiner.read(configMinerName)
+        def_config = configMiner["DEFAULT"]
+        minerAddr = def_config["miner_addr"]
+        serialPorts = def_config["serial_ports"]
+        tempoTrabalhar = int(def_config["time_work"])
+    except:
+        print("error read config file") 
+
+def writeConfigMiner():
+    def_config = configMiner["DEFAULT"]
+    def_config["miner_addr"] = minerAddr
+    def_config["serial_ports"] = serialPorts
+    try:
+        with open(configMinerName, 'w') as configfile:
+            configMiner.write(configfile)
+    except:
+        print("error write config file") 
+
+def miner( ith, s_port ):
+  global results_good
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  while True:
+    #Conecta com servidor TCP
     tcp_connected = False
     print("Connecting", HOST, PORT)
     try:
@@ -30,14 +62,15 @@ while True:
     
     #Conecta com iot via serial
     serial_connected = False
-    print("Connecting serial", serialPort)
+    print("Connecting serial", s_port)
     try:
-        ser = serial.Serial(f"{serialPort}", baudrate=115200, timeout=2.5)
+        ser = serial.Serial(f"{s_port}", baudrate=115200, timeout=2.5)
         time.sleep(2)
         serial_connected = True
         print("Serial connetecd")
     except:
         print("Serial error")
+        break
     
     time_wait = time.time() + 240
     while (time.time() < time_wait):
@@ -83,3 +116,39 @@ while True:
             if (result_response=="GOOD"):
                 results_good += 1
             print(result_response, results_good, hashrate )
+
+if __name__ == '__main__':
+	#Read config
+    readConfigMiner()
+    
+    #Get SiriCoin address
+    if ( minerAddr == "" ):
+        minerAddr = input(f"{Fore.YELLOW}Enter SiriCoin address: {Fore.WHITE}")
+    else:
+        print(f"{Fore.YELLOW}SiriCoin address:{Fore.WHITE}", minerAddr )
+        t_minerAddr = input(f"{Fore.YELLOW}Enter new SiriCoin address or ENTER to continue: {Fore.WHITE}")
+        if (t_minerAddr != ""):
+            minerAddr = t_minerAddr
+    
+    #Get list of serial ports
+    if ( serialPorts == "" ):
+        serialPorts = input(f"{Fore.YELLOW}Enter list serial ports: {Fore.WHITE}")
+    else:
+        print(f"{Fore.YELLOW}List serial ports:{Fore.WHITE}", serialPorts )
+        t_serialPorts = input(f"{Fore.YELLOW}Enter new list serial ports or ENTER to continue: {Fore.WHITE}")
+        if (t_serialPorts != ""):
+            serialPorts = t_serialPorts
+    
+    #Write config
+    writeConfigMiner()
+    
+    listPorts = serialPorts.split(',')
+    real_ports = list()
+    for port in listPorts:
+        if os.path.exists(port):
+            real_ports.append(port)
+    #Threads
+    index = 0
+    for port in real_ports:
+      Thread(target=miner, args=(index, port)).start()
+      index += 1
